5a3e506738cac141e63bdbc8d3190d74d68c5be1
Enoch Banjo <enoch@thegoodtill.com>
2020-03-04 14:29:42 +0000

    Task

--- src/App.vue
+++ src/App.vue
@@ -5,36 +5,36 @@
       color="deep-purple darken-4"
       dark
     >
-      <div class="d-flex align-center">
-        <v-img
+      <div class="d-flex align-center text-center">
+        <!-- <v-img
           alt="Logo"
           class="shrink mr-2"
           contain
           src="https://cdn.vuetifyjs.com/images/logos/vuetify-logo-dark.png"
           transition="scale-transition"
           width="20"
-        />
+        /> -->
 
-        <h4>Dataviz</h4> 
+        <h4>Catalog</h4> 
       </div>
 
       <v-spacer></v-spacer>
 
-      <v-tabs align-with-title class="pl-5">
+      <!-- <v-tabs align-with-title class="pl-5">
         <v-tab to='/'>Dashboard</v-tab>
-        <v-tab to='/editor'>Data Editor</v-tab>
-        <v-tab to='/search'>Smart Search</v-tab>
-      </v-tabs>
+         <v-tab to='/editor'>Data Editor</v-tab>
+        <v-tab to='/search'>Smart Search</v-tab> -->
+      <!-- </v-tabs> -->
      
 
-      <v-btn
+      <!-- <v-btn
         href="https://github.com/vuetifyjs/vuetify/releases/latest"
         target="_blank"
         text
       >
         <span class="mr-2">Latest Release</span>
         <v-icon>mdi-open-in-new</v-icon>
-      </v-btn>
+      </v-btn> -->
     </v-app-bar>
 
     <v-content>
